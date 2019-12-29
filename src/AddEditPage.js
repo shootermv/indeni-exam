dc20b991ba5cfb3896f2ca234843d7bb56336e1f
moshev <moshev@welldone-software.com>
2019-12-29 08:38:09 +0200

    checking validity of the form

--- src/AddEditPage.js
+++ src/AddEditPage.js
@@ -1,52 +1,105 @@
-import React, { useContext, useState } from 'react';
-import { UserContext } from './contexts/UserContext';
+import React, { useContext, useState, useRef } from "react";
+import { UserContext } from "./contexts/UserContext";
 import { Link } from "@reach/router";
 import { navigate } from "@reach/router";
 
-const getNewUser = () => ({avatar: 'http://placecorgi.com/300/300', fullName: '', email: '', address: '', birthday: ''});
+const getNewUser = () => ({
+  avatar: "http://placecorgi.com/300/300",
+  fullName: "",
+  email: "",
+  address: "",
+  birthday: "1998-07-15"
+});
+const convertToYYYYMMDD = birthday => {
+  let [month, day, year] = birthday.split("/");
+  day = `0${day}`.slice(-2);
+  month = `0${month}`.slice(-2);
+  return `${year}-${month}-${day}`;
+};
 
-export const AddEditPage = ({id}) => {
-    const isNew = id === 'new';
-    
-    const { users, dispatch } = useContext(UserContext);
-    users.find(user => id === user.id)
-    
-    const [user, setUser] = useState(isNew ? getNewUser : users.find(user => +id === user.id));
- 
-    const handleInputChange = event => {
-        const { name, value } = event.target
-    
-        setUser({ ...user, [name]: value })
+const getUser = (users, id) => {
+  const user = users.find(user => +id === user.id);
+  return Object.assign({}, user, {
+    birthday: convertToYYYYMMDD(user.birthday)
+  });
+};
+
+export const AddEditPage = ({ id }) => {
+  const isNew = id === "new";
+  const ref = useRef(null);
+  const { users, dispatch } = useContext(UserContext);
+  const [user, setUser] = useState(isNew ? getNewUser : getUser(users, id));
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+
+    setUser({ ...user, [name]: value });
+  };
+
+  const saveUser = e => {
+    e.preventDefault();
+    if (!ref.current.checkValidity()) {
+      // missing value
+      return;
     }
 
-    const saveUser = e => {
-      e.preventDefault();  
-      if (isNew) {
-        dispatch({type: 'ADD_USER', user});
-        navigate('/');
-        return;
-      }
-      dispatch({type: 'EDIT_USER', user});
-      navigate('/');
+    if (isNew) {
+      dispatch({ type: "ADD_USER", user });
+      navigate("/");
+      return;
     }
+    dispatch({ type: "EDIT_USER", user });
+    navigate("/");
+  };
 
-    return (
-        <>
-            <header className="main back">
-                <Link to="/">Back</Link>
-            </header>        
-            <form onSubmit={saveUser}>
-                
-                <img src={user.avatar} alt={user.fullName}/>
-                <div className="container">
-                    
-                    <div>Full Name:</div><div><input value={user.fullName} name="fullName" onChange={handleInputChange}/></div>
-                    <div>BirthDay:</div><div><input type="date"  value={user.birthday} name="birthday" onChange={handleInputChange}/></div>
-                    <div>Email:</div><div><input value={user.email} name="email" onChange={handleInputChange}/></div>
-                    <div>Address:</div><div><input value={user.address} name="address" onChange={handleInputChange}/></div>     
-                    <button>Save</button>
-                </div>
-            </form>
-        </>
-    );
+  return (
+    <>
+      <header className="main back">
+        <Link to="/">Back</Link>
+      </header>
+      <form onSubmit={saveUser} ref={ref}>
+        <img src={user.avatar} alt={user.fullName} />
+        <div className="container">
+          <div>Full Name:</div>
+          <div>
+            <input
+              value={user.fullName}
+              name="fullName"
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>BirthDay:</div>
+          <div>
+            <input
+              type="date"
+              value={user.birthday}
+              name="birthday"
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>Email:</div>
+          <div>
+            <input
+              value={user.email}
+              name="email"
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>Address:</div>
+          <div>
+            <input
+              value={user.address}
+              name="address"
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <button>Save</button>
+        </div>
+      </form>
+    </>
+  );
 };
