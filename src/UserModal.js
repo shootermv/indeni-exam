3515832789c27f3fa5f36bd6a60aa25805eead75
moshev <moshev@welldone-software.com>
2019-12-29 03:01:36 +0200

    styling modal

--- src/UserModal.js
+++ src/UserModal.js
@@ -4,19 +4,20 @@ import Modal from './shared/Modal';
 
 export const UserModal = ({ showModal, toggleModal, user }) => {
  
-    const { fullName } = user;
+    const { fullName, birthday, address, avatar, email } = user;
 
    
 
     return (showModal ? (<Modal closeMe={toggleModal}>   
         <header>
-          <h4>User {fullName}</h4>
+          <img src={avatar} alt={fullName}/>
           <button className="modal-close-btn" onClick={toggleModal}>x</button>
         </header>
-        <div className="modal-body modal-wrap">
-           Here will come details
-        </div>
-
-      
+        <div className="modal-body">
+           <div>Full Name:</div><div>{fullName}</div>
+           <div>BirthDay:</div><div>{birthday}</div>
+           <div>Email:</div><div>{email}</div>
+           <div>Address:</div><div>{address}</div>
+        </div> 
     </Modal>) : null);
 };
